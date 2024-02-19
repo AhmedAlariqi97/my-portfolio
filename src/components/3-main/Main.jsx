c61ac74355573050763d52bb052c39c4bca2e13f
unknown <ahmedalariqi97@gmail.com>
2024-02-19 20:54:50 +0300

    add main section

--- src/components/3-main/Main.jsx
+++ src/components/3-main/Main.jsx
@@ -11,22 +11,26 @@ const Main = () => {
         <button>Node & Express</button>
       </section>
 
-      <section className='right-section'>
-        <article className='card'>
-          <img src='./ahmed-cir.png' alt='' width={277} />
-          <div className='box' style={{width: "277px"}}>
-            <h2 className='title'>landing page</h2>
-            <p className='sub-title'> landing page landing pagd lakdjf landing page landing pagd lakdjf landing page landing pagd lakdjf</p>
-            <div className='flex icons'>
-              <div className='flex links'>
-                <div className='icon-link'></div>
-                <div className='icon-github-square'></div>
+      <section className='right-section flex'>
+        {["aa", "bb", "cc", "1", "7"].map((item) => {
+          return (
+            <article key={item} className='card'>
+              <img src='./ahmed-cir.png' alt='' width={277} />
+              <div className='box' style={{width: "277px"}}>
+                <h2 className='title'>landing page</h2>
+                <p className='sub-title'> landing page landing pagd lakdjf landing page landing pagd lakdjf landing page landing pagd lakdjf</p>
+                <div className='flex icons'>
+                  <div className='flex links'>
+                    <div className='icon-link'></div>
+                    <div className='icon-github-square'></div>
+                  </div>
+                  <a href='' className='more flex'>more
+                  <span className='icon-arrow-right'></span></a>
+                </div>
               </div>
-              <a href='' className='more flex'>more
-              <span className='icon-arrow-right'></span></a>
-            </div>
-          </div>
-        </article>
+            </article>
+          )
+        })}
       </section>
     </main>
   )
