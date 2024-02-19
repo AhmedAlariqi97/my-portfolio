b0d70bf4d07a986e61679b282e301aa857a9a44b
unknown <ahmedalariqi97@gmail.com>
2024-02-19 22:29:57 +0300

    add contact us section

--- src/components/4-contact/Contact.jsx
+++ src/components/4-contact/Contact.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import './contact.css'
 
 const Contact = () => {
   return (
-    <div>contact</div>
+    <section className='contact-us'>
+      <div className='head'>
+        <h2 className='title'>
+          <span className='icon-envelope'></span>
+          Contact Us
+        </h2>
+        <p className='sub-title'>
+          Contact us for more information and get notified
+        </p>
+      </div>
+      <div className='body flex'>
+        <div className='left-section'>
+          <form className='form'>
+           <div className='flex'>
+            <label htmlFor='email'>Email Address:</label>
+            <input required type='email' name='' id='email' />
+           </div>
+
+           <div className='flex' style={{marginTop: "24px"}}>
+            <label htmlFor='message'>Your Message:</label>
+            <textarea required name='' id='message'></textarea>
+           </div>
+
+            <button type='submit' className='submit'>Submit</button>
+          </form>
+        </div>
+        <div className='right-section animation'>
+            animate
+        </div>
+      </div>
+    </section>
   )
 }
 
