7e4b88caedb32a19246305c9bd8876c2f74de1ca
unknown <ahmedalariqi97@gmail.com>
2024-02-19 20:12:17 +0300

    add main section

--- src/components/3-main/Main.jsx
+++ src/components/3-main/Main.jsx
@@ -4,7 +4,7 @@ const Main = () => {
   return (
     <main className='main flex'>
       <section className='left-section flex'>
-        <button>all project</button>
+        <button className='active'>all project</button>
         <button>Html & Css</button>
         <button>JavaScript</button>
         <button>React & Next</button>
@@ -12,7 +12,21 @@ const Main = () => {
       </section>
 
       <section className='right-section'>
-        ddddddddddddddd
+        <article className='card'>
+          <img src='./ahmed-cir.png' alt='' width={277} />
+          <div className='box' style={{width: "277px"}}>
+            <h2 className='title'>landing page</h2>
+            <p className='sub-title'> landing page landing pagd lakdjf landing page landing pagd lakdjf landing page landing pagd lakdjf</p>
+            <div className='flex icons'>
+              <div className='flex links'>
+                <div className='icon-link'></div>
+                <div className='icon-github-square'></div>
+              </div>
+              <a href='' className='more flex'>more
+              <span className='icon-arrow-right'></span></a>
+            </div>
+          </div>
+        </article>
       </section>
     </main>
   )
