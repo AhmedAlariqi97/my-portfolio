9166c82fcdc015112e0f705bc1731f245b685c9b
unknown <ahmedalariqi97@gmail.com>
2024-02-24 23:55:47 +0300

    add allord images

--- src/components/4-main/myProjects.js
+++ src/components/4-main/myProjects.js
@@ -1,6 +1,6 @@
 
 export const myProjects = [
-    {title: "react project", category: "react", descreption: "akldjf akldjf akldjf alkjdf alkdjf akjdfk akldjf", imgPath: "./" },
+    {title: "Al-lord Group", category: "html", descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./al-lord.png" },
     {title: "html & css project", category: "html", descreption: "akldjf akldjf akldjf alkjdf alkdjf akjdfk akldjf", imgPath: "./" },
     {title: "javascript project", category: "javascript", descreption: "akldjf akldjf akldjf alkjdf alkdjf akjdfk akldjf", imgPath: "./" },
     {title: "node project", category: "node", descreption: "akldjf akldjf akldjf alkjdf alkdjf akjdfk akldjf", imgPath: "./" }
