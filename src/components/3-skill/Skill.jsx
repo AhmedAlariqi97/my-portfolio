89577c8110e3465d40c41662ad72744005670dfd
unknown <ahmedalariqi97@gmail.com>
2024-02-24 21:15:24 +0300

    add my skills files

--- src/components/3-skill/Skill.jsx
+++ src/components/3-skill/Skill.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import './skill.css'
+import { mySkills } from './mySkills'
+import { AnimatePresence, motion } from 'framer-motion'
 
 const Skill = () => {
   return (
     <section className='skill'>
         <div className='head'>
             <h2 className='title'>Skills</h2>
+        </div>
+        <div className='body'>
+            <AnimatePresence>
+                {mySkills.map((item) => {
+                return (
+                    <motion.article
+                    layout
+                    initial={{ transform: "scale(0)" }}
+                    animate={{ transform: "scale(1)" }}
+                    transition={{ type: "spring", damping: 7, stiffness: 50}}
+                    
+                    key={item.imgPath} className='card'>
+                   
+                    <div className='box' style={{ width: "277px" }}>
+                        <h2 className='title'>{item.title}</h2>
+                    </div>
+                    </motion.article>
+                )
+                })}
+            </AnimatePresence>
         </div>
     </section>
   )
